2e9a7864848be08c84ee3f7e6286afda43cdc8ae
shaannessy25 <shaan.hurley25@gmail.com>
2020-09-03 15:40:15 -0700

    adding store and provider

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,20 @@ import React, { Component } from 'react'
 
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducers from '../../reducers/'
+import reducers from '../reducers/'
 import './App.css';
 
-const store = createStore()
+const store = createStore(reducers)
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Provider store={store}>
+        <div>
+          <h1>Hello World</h1>
+        </div>
+      </Provider>
     )
   }
 }
